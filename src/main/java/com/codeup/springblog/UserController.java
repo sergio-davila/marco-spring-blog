b0a78d55f4444c71225386a1f8aa2066a60ae642
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-06-10 12:00:11 -0500

    feat: working on spring views exercise

---
+++ src/main/java/com/codeup/springblog/UserController.java
@@ -0,0 +1,20 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/users/all")
+    public String getAllUsers() {
+        return "users";
+    }
+
+    @GetMapping("/user")
+    @ResponseBody
+    public String getTheStringUser(){
+        return "users";
+    }
+}

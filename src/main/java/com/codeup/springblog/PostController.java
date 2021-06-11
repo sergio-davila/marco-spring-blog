0b5a15a09ef2813e71632ffea5a217c161e907ec
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-06-11 11:07:08 -0500

    feat: done with views exercise completely

--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -1,23 +1,40 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
 
+
+
     @GetMapping("/posts")
-    @ResponseBody
-    public String posts() {
-        return "Welcome to the posts index page!";
+    public String posts(
+            Model model
+    ) {
+        List<Post> listOfPosts = new ArrayList<>();
+        listOfPosts.add(new Post("TV", "60 inch tv"));
+        listOfPosts.add(new Post("Laptop", "Gaming laptop"));
+        model.addAttribute("posts", listOfPosts);
+
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String postsId(@PathVariable int id) {
-        return "This is your ID: " + id;
+    public String postsId(
+            @PathVariable int id,
+            Model model
+    ) {
+        Post post = new Post("TV", "60 inch tv");
+        model.addAttribute("singlePost", post);
+
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")

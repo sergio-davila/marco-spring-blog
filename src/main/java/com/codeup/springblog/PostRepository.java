9db6520113a5512e52b9cd1652c053b78aa60be0
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-06-13 17:17:29 -0500

    feat: added header to spring blog

--- src/main/java/com/codeup/springblog/PostRepository.java
+++ src/main/java/com/codeup/springblog/PostRepository.java
@@ -6,5 +6,4 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     Post findByTitle(String title); // select * from ads where title = ?
     Post findByTitleLike(String searchPattern); // select * from ads where title LIKE = ?
     Post findFirstByTitle(String title); // select * from ads where title = ? limit 1
-
 }

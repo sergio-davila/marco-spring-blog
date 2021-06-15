abb3baf63ebb14e618537c9f9b301f77e4ecd2bb
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-06-15 14:23:54 -0500

    feat: finished refactoring for form model binding exercise

--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -21,10 +21,6 @@ public class PostController {
 
     @GetMapping("/posts")
     public String showAllPosts(Model model) {
-//        List<Post> listOfPosts = new ArrayList<>();
-//        listOfPosts.add(new Post("TV", "60 inch tv"));
-//        listOfPosts.add(new Post("Laptop", "Gaming laptop"));
-//        model.addAttribute("posts", listOfPosts);
 
         model.addAttribute("posts", postDao.findAll());
 
@@ -33,9 +29,6 @@ public class PostController {
 
     @GetMapping("/posts/{id}")
     public String singlePostPage(@PathVariable long id, Model model) {
-//        Post post = new Post("TV", "60 inch tv");
-//        model.addAttribute("singlePost", adDao.findByTitle());
-//        model.addAttribute("singlePost", postDao.findByTitle("TV"));
         model.addAttribute("singlePost", postDao.getById(id));
         return "posts/show";
     }
@@ -77,11 +70,8 @@ public class PostController {
 
 //    @PostMapping("/posts/edit/{id}")
 //    public String postsEdit(@PathVariable long id, @RequestParam(name = "title") String title, @RequestParam(name = "body") String body, @RequestParam(name = "imageURL") String imageURL, @RequestParam(name = "imageDescription") String imageDescription) {
-        @PostMapping("/posts/edit/{id}")
-        public String postsEdit(@PathVariable long id, @ModelAttribute Post post, @RequestParam(name = "imageURL") String imageURL, @RequestParam(name = "imageDescription") String imageDescription) {
-
-//        PostImage image1 = new PostImage("https://i.ytimg.com/vi/ggNCKiu7nhA/maxresdefault.jpg", "60 in tv", post);
-//        PostImage image2 = new PostImage("https://i.rtings.com/assets/products/mrQV6IMB/samsung-q60-q60t-qled/design-small.jpg", "60 in tv train", post);
+    @PostMapping("/posts/edit/{id}")
+    public String postsEdit(@PathVariable long id, @ModelAttribute Post post, @RequestParam(name = "imageURL") String imageURL, @RequestParam(name = "imageDescription") String imageDescription) {
         PostImage image1 = new PostImage(imageURL, imageDescription, post);
         List<PostImage> images = new ArrayList<>();
         images.add(image1);

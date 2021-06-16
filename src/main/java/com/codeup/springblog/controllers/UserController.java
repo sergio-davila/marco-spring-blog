770295726fe5af77bab197812a6a11866dd41751
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-06-16 12:21:43 -0500

    feat: worked on security lecture

--- src/main/java/com/codeup/springblog/controllers/UserController.java
+++ src/main/java/com/codeup/springblog/controllers/UserController.java
@@ -1,36 +1,38 @@
 package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.DAOs.UserRepository;
+import com.codeup.springblog.models.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
 
     private UserRepository users;
-//    private PasswordEncoder passwordEncoder;
-//
-//    public UserController(UserRepository users, PasswordEncoder passwordEncoder) {
-//        this.users = users;
-//        this.passwordEncoder = passwordEncoder;
-//    }
+    private PasswordEncoder passwordEncoder;
 
-    @GetMapping("/sign-up")
-    public String showSignUpForm(Model model) {
-        model.addAttribute()
+    public UserController(UserRepository users, PasswordEncoder passwordEncoder) {
+        this.users = users;
+        this.passwordEncoder = passwordEncoder;
     }
 
-//    @GetMapping("/users/all")
-//    public String getAllUsers() {
-//        return "users";
-//    }
-//
-//    @GetMapping("/user")
-//    @ResponseBody
-//    public String getTheStringUser(){
-//        return "users";
-//    }
+    @GetMapping("/sign-up")
+    public String showSignupForm(Model model){
+        model.addAttribute("user", new User());
+        return "users/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String saveUser(@ModelAttribute User user){
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        users.save(user);
+        return "redirect:/login";
+    }
+
 }
